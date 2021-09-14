b5d8af567bc9aa3ee128394738458d98ca4c5b44
ashereskind <90290688+ashereskind@users.noreply.github.com>
2021-09-14 10:08:12 -0600

    Update main.js

--- unit-2/main.js
+++ unit-2/main.js
@@ -54,6 +54,35 @@ function cities(){
 
         table.appendChild(tr);
     };
+    
+        //Example 2.4 line 25...loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        var tr = document.createElement("tr");
+
+        var city = document.createElement("td");
+        //first conditional block
+            if (cityPop[i].city == 'Madison'){
+                city.innerHTML = 'Badgerville';
+            } else if (cityPop[i].city == 'Green Bay'){
+                city.innerHTML = 'Packerville';
+            } else {
+                city.innerHTML = cityPop[i].city;
+            }
+
+            tr.appendChild(city);
+
+            var pop = document.createElement("td");
+        //second conditional block        
+            if (cityPop[i].population < 500000){
+                pop.innerHTML = cityPop[i].population;
+            } else {
+                pop.innerHTML = 'Too big!';
+            };
+
+        tr.appendChild(pop);
+        table.appendChild(tr);
+    };
+
 
     //add the table to the div in index.html
     var mydiv = document.getElementById("mydiv");
